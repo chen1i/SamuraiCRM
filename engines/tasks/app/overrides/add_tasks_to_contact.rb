7f7691cd853c3ec5a2c9a09ec2ad51790e360f49
Li Chen <kyrrechen@gmail.com>
2015-08-22 07:50:05 +0800

    fix bug, new task should not attach to other's contacts

--- engines/tasks/app/overrides/add_tasks_to_contact.rb
+++ engines/tasks/app/overrides/add_tasks_to_contact.rb
@@ -3,4 +3,4 @@ Deface::Override.new(virtual_path: 'samurai/contacts/contacts/show',
                      insert_after: "[data-samurai-hook='contacts_show']",
                      partial: 'overrides/contact_tasks_list',
                      namespaced: true,
-                     original: 'f5fe48b6dc6986328e0b873b3ffa1b228dd52a7c')
+                     original: '98a78123a4c3e487a16a47f0ed2ded2a8b5bed73')
